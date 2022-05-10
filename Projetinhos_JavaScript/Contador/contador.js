31889a49876176b87d09ce2520cb4f521b52bd50
unknown <eduardo2018ads@hotmail.com>
2022-05-10 00:03:24 -0700

    code update

--- Projetinhos_JavaScript/Contador/contador.js
+++ Projetinhos_JavaScript/Contador/contador.js
@@ -4,10 +4,10 @@ resultado.addEventListener('click', contador)
 
 function contador(){
 
-    let inicio = document.querySelector("#txti")
+    let inicio = document.querySelector("#txti");
     let fim = document.querySelector("#txtf");
     let passo = document.querySelector("#txtp");
-    let resultado = document.querySelector("#res")
+   
 
 
     if(inicio.value.length == 0 ||  fim.value.length == 0 || passo.value.length == 0){
@@ -25,7 +25,7 @@ function contador(){
             
             res.innerHTML += `${cont} \u{1F449}`
                 
-        }       
+        }     
             res.innerHTML += `\u{1F3C1}`
     }    
 }
